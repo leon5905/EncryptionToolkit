ff39d8514209d6ea1b3e9123cd6c1a09d19896fa
yeohw <yeohw@WZHU-SURFACE>
2017-05-28 14:01:30 +0800

    Commintg

--- EncryptionToolkit/Main.cpp
+++ EncryptionToolkit/Main.cpp
@@ -9,6 +9,7 @@ using namespace Encryption;
 
 string string_to_hex(const string&);
 //string hex_to_string(const string& input);
+//MErge branch 123
 
 int main() {
 	cout << "Encryption Toolkit Demo \n\n";
